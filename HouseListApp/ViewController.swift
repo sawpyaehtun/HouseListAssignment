ad662c2254f9b7350ea69a5ffda72ca3b3463c64
sawpyae <sawpyae.htun@haulio.io>
2019-09-11 14:52:01 +0630

    Initial Commit

---
+++ HouseListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HouseListApp
+//
+//  Created by saw pyaehtun on 11/09/2019.
+//  Copyright © 2019 saw pyaehtun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
